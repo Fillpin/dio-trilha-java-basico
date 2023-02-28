df6daada26f7d6242a4a020538f8d4a9e0f9a784
Fillpin <fillipearaujoo97@gmail.com>
2023-02-28 12:32:17 -0300

    Estudos de Sintaxe Java - Tipos de Variáveis

---
+++ src/TiposVariaveis.java
@@ -0,0 +1,18 @@
+public class TiposVariaveis {
+    public static void main(String[] args) {
+        final String nome = "Jordan";
+        final String sobrenome = "Peterson";
+        int idade = 25;
+        String endereco = "Tv. Capilé, nº";
+        short numEndereco = 53;
+        double salario = 1450.33;
+        float peso = 68.3F;
+
+        System.out.println("Nome completo: " + nome + " " + sobrenome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Peso: " + peso + "Kg");
+        System.out.println("Endereço completo: " + endereco + " " + numEndereco);
+        System.out.println("Salário: R$" + salario);
+
+    }
+}
