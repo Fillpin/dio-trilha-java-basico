a5eb987e1c658656a4e821aa4df621ef76d0ccbe
Fillpin <fillipearaujoo97@gmail.com>
2023-02-28 14:20:39 -0300

    Estudos de Sintaxe Java - Tipos de Variáveis

---
+++ src/OperadoresJava.java
@@ -0,0 +1,15 @@
+public class OperadoresJava {
+    public static void main(String[] args) {
+        // valor original é sempre positivo
+        int numero = 5;
+        System.out.println(numero);
+
+        // deixar o número negativo
+        numero = - numero;
+        System.out.println(numero);
+
+        //converter números negativos em positivos (multiplicar por -1)
+        numero = numero * -1;
+        System.out.println(numero);
+    }
+}
