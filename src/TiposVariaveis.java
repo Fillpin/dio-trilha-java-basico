db54d041706bac8c98f8fbb9393d91238a30aca9
Fillpin <fillipearaujoo97@gmail.com>
2023-02-28 12:38:46 -0300

    Estudos de Sintaxe Java - Tipos de Variáveis

--- src/TiposVariaveis.java
+++ src/TiposVariaveis.java
@@ -1,11 +1,22 @@
 public class TiposVariaveis {
     public static void main(String[] args) {
+        // A palavra "final" antes do tipo da variável indica um tipo de variável constante e, portanto, inalterável.
         final String nome = "Jordan";
         final String sobrenome = "Peterson";
+
+        // int = valores inteiros
         int idade = 25;
+
+        // String = valores em texto
         String endereco = "Tv. Capilé, nº";
+
+        // short = representa valores de -32768 a 32767
         short numEndereco = 53;
+
+        // doule = números decimais
         double salario = 1450.33;
+
+        // float = números decimais (obrigatório o uso do "F" maiúsculo no final do número
         float peso = 68.3F;
 
         System.out.println("Nome completo: " + nome + " " + sobrenome);
