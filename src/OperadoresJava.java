9e983c3280047fe90b8ce6e4533f8beffe5ac964
Fillpin <fillipearaujoo97@gmail.com>
2023-03-01 09:21:42 -0300

    Estudos de Sintaxe Java - Tipos de Variáveis

--- src/OperadoresJava.java
+++ src/OperadoresJava.java
@@ -29,6 +29,26 @@ public class OperadoresJava {
         variavel = !variavel;
         System.out.println(variavel);
 
+        //operador ternário
+        int a = 5;
+        int b = 7;
 
+        // tipo pode ser String ou int ; expressão booleana (true ou false) símbolo "?" valor se true : valor se false;
+        String resultado = a==b ? "verdadeiro" : "falso";
+        System.out.println(resultado);
+
+        //operadores relacionais
+        /* == comparador se dois valores são iguais
+        * != comparador se dois valores são diferentes
+        * >= maior ou igual a
+        * <= menor ou igual a
+        * > maior que
+        * < menor que
+        * */
+
+        /* operadores lógicos
+        * && operador lógico "E" as duas condições devem ser verdadeiras
+        * || operador lógico "OU" uma condição deve ser verdadeira
+         */
     }
 }
